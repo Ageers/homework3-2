809b881fd79ff97e12a42f608206120a32a43edd
Ageers <Ageers@mail.ru>
2025-09-05 20:17:56 +0300

    this 4 task

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,26 @@
+import java.util.Scanner;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        Scanner sc = new Scanner(System.in);
+/* // Среднее арифметическое N чисел Ввод: N, затем N целых. Вывести: среднее с точностью, например, до 3 знаков.
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+
+        int n = sc.nextInt();
+        double sum = 0;
+        double average = 0;
+        for (int i = 0; i < n; i++){
+            double inputN = sc.nextInt();
+            sum += inputN;
         }
+        average = sum / n;
+        String result = String.format("%.3f",average);
+        System.out.println(result);
+ */
+
+
+
     }
 }
